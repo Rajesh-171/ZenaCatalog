094147ca6443e9efc9c4a6c6f72895ca6ea7ef86
Rajesh-171 <rajeshreddy171@gmail.com>
2024-07-29 14:21:01 +0530

    Update streamlit_zenacatalog_app.py
    import col

--- streamlit_zenacatalog_app.py
+++ streamlit_zenacatalog_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+#from snowflake.snowpark.functions import col
 import pandas as pd
 
 st.title("Zena's Amazing Athleisure Catalog")
